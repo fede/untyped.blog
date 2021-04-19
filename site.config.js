df03c8a1ec2e8023dd51cc1ebd03e3ffc7b9eb44
Federico Ratier <federico.ratier@klarna.com>
2021-04-19 07:42:52 +0200

    Init

---
+++ site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: `Untyped`,
+    titleSpan: `blog`,
+    description: `Collection of unfinished notes ordered by last update first instead of creation date.`,
+    siteUrl: `https://untyped.blog`,
+    author: {
+    name: "Fede Ratier",
+    linkedin: "https://www.linkedin.com/in/federatier/",
+    twitter: "https://www.twitter.com/federatier/"
+    }
+}
